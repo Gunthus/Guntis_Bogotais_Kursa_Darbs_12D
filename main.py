b5d400452df0f0d9ba79afb5c3afaf65905464a2
GuntisB <56440230+Gunthus@users.noreply.github.com>
2022-04-18 01:52:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,259 +1,106 @@
 import random
-import os
-import time
- 
-# The Card class definition
-class Card:
-    def __init__(self, suit, value, card_value):
-         
+
+class Card(object):
+    def __init__(self, suit, val):
         self.suit = suit
-        self.value = value
-        self.card_value = card_value
- 
-# Clear the terminal
-def clear():
-    os.system("clear")
- 
-# Function to print the cards
-def print_cards(cards, hidden):
-         
-    s = ""
-    for card in cards:
-        if card.value == '10':
-            s = s + "\t| {} |".format(card.value)
+        self.value = val
+
+    # Implementing build in methods so that you can print a card object
+    def __unicode__(self):
+        return self.show()
+    def __str__(self):
+        return self.show()
+    def __repr__(self):
+        return self.show()
+        
+    def show(self):
+        if self.value == 1:
+            val = "Ace"
+        elif self.value == 11:
+            val = "Jack"
+        elif self.value == 12:
+            val = "Queen"
+        elif self.value == 13:
+            val = "King"
         else:
-            s = s + "\t| {} |".format(card.value)  
-    if hidden:
-        s += "\t|        |"    
-    print(s)
- 
- 
-    s = ""
-    for card in cards:
-        s = s + "\t| {} |".format(card.suit)
-    if hidden:
-        s += "\t| Hidden |"
-    print(s)    
-    print()
- 
-# Function for a single game of blackjack
-def blackjack_game(deck):
-    player_cards = []
-    dealer_cards = []
-    player_score = 0
-    dealer_score = 0
-    clear()
- 
-    # Initial dealing for player and dealer
-    while len(player_cards) < 2:
- 
-        # Randomly dealing a card
-        player_card = random.choice(deck)
-        player_cards.append(player_card)
-        deck.remove(player_card)
- 
-        # Updating the player score
-        player_score += player_card.card_value
- 
-        # In case both the cards are Ace, make the first ace value as 1 
-        if len(player_cards) == 2:
-            if player_cards[0].card_value == 11 and player_cards[1].card_value == 11:
-                player_cards[0].card_value = 1
-                player_score -= 10
- 
-        # Print player cards and score      
-        print("PLAYER CARDS: ")
-        print_cards(player_cards, False)
-        print("PLAYER SCORE = ", player_score)
- 
-        input()
- 
-        # Randomly dealing a card
-        dealer_card = random.choice(deck)
-        dealer_cards.append(dealer_card)
-        deck.remove(dealer_card)
- 
-        # Updating the dealer score
-        dealer_score += dealer_card.card_value
- 
-        # Print dealer cards and score, keeping in mind to hide the second card and score
-        print("DEALER CARDS: ")
-        if len(dealer_cards) == 1:
-            print_cards(dealer_cards, False)
-            print("DEALER SCORE = ", dealer_score)
-        else:
-            print_cards(dealer_cards[:-1], True)    
-            print("DEALER SCORE = ", dealer_score - dealer_cards[-1].card_value)
- 
- 
-        # In case both the cards are Ace, make the second ace value as 1 
-        if len(dealer_cards) == 2:
-            if dealer_cards[0].card_value == 11 and dealer_cards[1].card_value == 11:
-                dealer_cards[1].card_value = 1
-                dealer_score -= 10
- 
-        input()
- 
-    # Player gets a blackjack   
-    if player_score == 21:
-        print("PLAYER HAS A BLACKJACK!!!!")
-        print("PLAYER WINS!!!!")
-        quit()
- 
-    clear()
- 
-    # Print dealer and player cards
-    print("DEALER CARDS: ")
-    print_cards(dealer_cards[:-1], True)
-    print("DEALER SCORE = ", dealer_score - dealer_cards[-1].card_value)
- 
-    print() 
- 
-    print("PLAYER CARDS: ")
-    print_cards(player_cards, False)
-    print("PLAYER SCORE = ", player_score)
- 
-    # Managing the player moves
-    while player_score < 21:
-        choice = input("Enter H to Hit or S to Stand : ")
- 
-        # Sanity checks for player's choice
-        if len(choice) != 1 or (choice.upper() != 'H' and choice.upper() != 'S'):
-            clear()
-            print("Wrong choice!! Try Again")
- 
-        # If player decides to HIT
-        if choice.upper() == 'H':
- 
-            # Dealing a new card
-            player_card = random.choice(deck)
-            player_cards.append(player_card)
-            deck.remove(player_card)
- 
-            # Updating player score
-            player_score += player_card.card_value
- 
-            # Updating player score in case player's card have ace in them
-            c = 0
-            while player_score > 21 and c < len(player_cards):
-                if player_cards[c].card_value == 11:
-                    player_cards[c].card_value = 1
-                    player_score -= 10
-                    c += 1
-                else:
-                    c += 1 
- 
-            clear()     
- 
-            # Print player and dealer cards
-            print("DEALER CARDS: ")
-            print_cards(dealer_cards[:-1], True)
-            print("DEALER SCORE = ", dealer_score - dealer_cards[-1].card_value)
- 
-            print()
- 
-            print("PLAYER CARDS: ")
-            print_cards(player_cards, False)
-            print("PLAYER SCORE = ", player_score)
-             
-        # If player decides to Stand
-        if choice.upper() == 'S':
-            break
- 
- 
-    clear() 
- 
-    # Print player and dealer cards
-    print("PLAYER CARDS: ")
-    print_cards(player_cards, False)
-    print("PLAYER SCORE = ", player_score)
- 
-    print()
-    print("DEALER IS REVEALING THE CARDS....")
- 
-    print("DEALER CARDS: ")
-    print_cards(dealer_cards, False)
-    print("DEALER SCORE = ", dealer_score)
- 
-    # Check if player has a Blackjack
-    if player_score == 21:
-        print("PLAYER HAS A BLACKJACK")
-        quit()
- 
-    # Check if player busts
-    if player_score > 21:
-        print("PLAYER BUSTED!!! GAME OVER!!!")
-        quit()
- 
-    input() 
- 
-    # Managing the dealer moves
-    while dealer_score < 17:
-        clear() 
- 
-        print("DEALER DECIDES TO HIT.....")
- 
-        # Dealing card for dealer
-        dealer_card = random.choice(deck)
-        dealer_cards.append(dealer_card)
-        deck.remove(dealer_card)
- 
-        # Updating the dealer's score
-        dealer_score += dealer_card.card_value
- 
-        # Updating player score in case player's card have ace in them
-        c = 0
-        while dealer_score > 21 and c < len(dealer_cards):
-            if dealer_cards[c].card_value == 11:
-                dealer_cards[c].card_value = 1
-                dealer_score -= 10
-                c += 1
-            else:
-                c += 1
- 
-        # print player and dealer cards
-        print("PLAYER CARDS: ")
-        print_cards(player_cards, False)
-        print("PLAYER SCORE = ", player_score)
- 
-        print()
- 
-        print("DEALER CARDS: ")
-        print_cards(dealer_cards, False)
-        print("DEALER SCORE = ", dealer_score)      
- 
-        input()
- 
- 
-    # Dealer busts
-    if dealer_score > 21:        
-        print("DEALER BUSTED!!! YOU WIN!!!") 
-        quit()  
-    # Dealer gets a blackjack
-    if dealer_score == 21:
-        print("DEALER HAS A BLACKJACK!!! PLAYER LOSES")
-        quit()
-    # TIE Game
-    if dealer_score == player_score:
-        print("TIE GAME!!!!")
-    # Player Wins
-    elif player_score > dealer_score:
-        print("PLAYER WINS!!!")                 
-    # Dealer Wins
-    else:
-        print("DEALER WINS!!!")                 
- 
-if __name__ == '__main__':
- 
-    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-    suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
-    cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    cards_values = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
- 
-    deck = []
- 
-    for suit in suits:
-        for card in cards:
-            deck.append(Card(suits_values[suit], card, cards_values[card]))
-     
-    blackjack_game(deck)
+            val = self.value
+
+        return "{} of {}".format(val, self.suit)
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    # Display all cards in the deck
+    def show(self):
+        for card in self.cards:
+            print (card.show())
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            for val in range(1,14):
+                self.cards.append(Card(suit, val))
+
+    # Shuffle the deck
+    def shuffle(self, num=1):
+        length = len(self.cards)
+        for _ in range(num):
+            # This is the fisher yates shuffle algorithm
+            for i in range(length-1, 0, -1):
+                randi = random.randint(0, i)
+                if i == randi:
+                    continue
+                self.cards[i], self.cards[randi] = self.cards[randi], self.cards[i]
+            # You can also use the build in shuffle method
+            # random.shuffle(self.cards)
+
+    # Return the top card
+    def deal(self):
+        return self.cards.pop()
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def sayHello(self):
+        print ("Hi! My name is {}".format(self.name))
+        return self
+
+    # Draw n number of cards from a deck
+    # Returns true in n cards are drawn, false if less then that
+    def draw(self, deck, num=1):
+        for _ in range(num):
+            card = deck.deal()
+            if card:
+                self.hand.append(card)
+            else: 
+                return False
+        return True
+
+    # Display all the cards in the players hand
+    def showHand(self):
+        print ("{}'s hand: {}".format(self.name, self.hand))
+        return self
+
+    def discard(self):
+        return self.hand.pop()
+
+# Test making a Card
+# card = Card('Spades', 6)
+# print card
+
+# Test making a Deck
+myDeck = Deck()
+myDeck.shuffle()
+# deck.show()
+
+bob = Player("Bob")
+bob.sayHello()
+bob.draw(myDeck, 5)
+bob.showHand()
